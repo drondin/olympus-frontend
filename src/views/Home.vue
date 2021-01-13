ee125ec3d0c098d00c127f2cda3dfee8e4b641f6
Point Blank <pointblank.connect@gmail.com>
2021-01-13 00:23:52 +0000

    Eth -> Dai

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="block">
     <h1 class="mb-4 main-title">OLYMPUS</h1>
     <p class="mb-4"><b class="warn">This is a private presale.</b>  If you have not been invited, your transaction will fail and waste your transaction fee!</p>
-    <p class="mb-2">Cap Reached in {{ $store.state.settings.remainingEth }} ETH</p>
+    <p class="mb-2">Cap Reached in {{ $store.state.settings.remainingEth }} DAI</p>
     <p class="mb-2">
       Balance: <span class="hasEffect" @click="maxStake">{{ $store.state.settings.balance }}</span>
     </p>
