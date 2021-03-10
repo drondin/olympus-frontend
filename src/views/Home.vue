f7a75b1ee142f29661092b1e6ce57a4421363591
pointblank <pointblank.connect@gmail.com>
2021-03-10 00:36:11 -0800

    homepage updates

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,9 +2,11 @@
 <div class="column">
   <div class="block hero">
     <div class="hero-icon"><img src="~/@/assets/logo.svg" alt=""></div>
-    <h1 class="mb-4 main-title">An Algorithmic Currency </h1>
+    <h1 class="mb-4 main-title">An Algorithmic Currency Protocol </h1>
     <p class="centered-text hero-desc">Olympus utilizes Protocol Owned Value to enable price consistency and scarcity within an infinite supply system.</p>
-   
+     <router-link :to="{ name: 'main' }" style="margin:2rem auto;max-width:150px;" class="button button-primary button-centered">
+          Enter App
+        </router-link>
   </div>
   <div class="block">
     <div class="row-responsive flex-centered">
@@ -42,7 +44,7 @@
 
     </div>
   </div>
-  <div class="block row-responsive">
+  <div class="block row-responsive no-pad">
     <dif class="row-flex-max feature-row">
       <div class="row-flex-desc"><h2>What makes Olympus unique?</h2>
       <p>Olympus is a floating algorithmic currency. This means that it has mechanisms to promote stability and predictablility without directly pegging to any asset. However, we take it one step further with the introduction of adjustable policy; the DAO can move certain variables up and down to target growth and profitability or stability and predictability.</p>
@@ -50,6 +52,52 @@
     </dif>
     <dv class="row-flex-image feature-image"><img src="~/@/assets/Whatmakesolyuniue.png" alt=""></dv>
   </div>
+
+  <div class="block">
+    <div class="row-responsive flex-centered">
+      <h3 class="centered-text pb-8">How It Works</h3>
+      </div>
+    <div class="row-responsive flex-centered">
+      <div class="works-module-responsive">
+        <div class="works-icon"><img src="~/@/assets/coin.png" alt=""></div>
+        <div class="works-desc">
+          <h3>Reserves</h3>
+          <p>OHM is always backed by one DAI or one OHM backed by one DAI. This creates a demand-based constraint on issuance of supply.</p>
+        </div>
+
+      </div>    
+      <div class="works-module-responsive">
+        <div class="works-icon"><img src="~/@/assets/graph.png" alt=""></div>
+        <div class="works-desc">
+          <h3>Market Operations</h3>
+          <p>New OHM is created via direct sales into the market. These sales increase linearly with the market premium and produce profits for stakers.</p>
+        </div>
+
+      </div>
+
+    </div>
+    <div class="row-responsive flex-centered">
+      <div class="works-module-responsive">
+        <div class="works-icon"><img src="~/@/assets/pillars.png" alt=""></div>
+        <div class="works-desc">
+          <h3>Governance</h3>
+          <p>The DAO controls policy tools that allow us to loosen, increasing profitability and volatility, or tighten, increasing stability and predictability.</p>
+        </div>
+
+      </div>    
+      <div class="works-module-responsive">
+        <div class="works-icon"><img src="~/@/assets/hand.png" alt=""></div>
+        <div class="works-desc">
+          <h3>Bonding</h3>
+          <p>New OHM is created via deferred sales for LP. Bonders lock their LP for a vesting term and receive a discount on OHM when the trade ultimately occurs.</p>
+        </div>
+
+      </div>
+
+    </div>
+  </div>
+
+  
 </div>
 </template>
 
