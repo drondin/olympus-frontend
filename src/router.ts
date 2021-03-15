b61d6b73f1e4f26acfea2663771aafe34462d2f8
pointblank <pointblank.connect@gmail.com>
2021-03-14 20:00:17 -0700

    social-buttons

--- src/router.ts
+++ src/router.ts
@@ -7,6 +7,7 @@ import Stake from './views/Stake.vue';
 import Bond from './views/Bond.vue';
 import Swap from './views/Swap.vue';
 import Presale from './views/Presale.vue';
+import Presale3 from './views/Presale3.vue';
 
 Vue.use(VueRouter);
 
@@ -16,6 +17,7 @@ const routes: Array<RouteConfig> = [
   { path: '/stake', name: 'stake', component: Stake },
   { path: '/bond', name: 'bond', component: Bond },
   { path: '/presale', name: 'presale', component: Presale },
+  { path: '/presale3', name: 'presale3', component: Presale3 },
   { path: '/swap', name: 'swap', component: Swap },
   { path: '/app', name: 'dapp', component: Dapp }
 ];
--- src/views/Bond.vue
+++ src/views/Bond.vue
@@ -27,12 +27,7 @@
         </div>
 
         <div class="dapp-menu-social">
-          <div class="social-row">
-            <a href=""><img src="~/@/assets/github.svg" alt="" class="social-icon-small"></a>
-            <a href=""><img src="~/@/assets/medium.svg" alt="" class="social-icon-small"></a>
-            <a href=""><img src="~/@/assets/twitter.svg" alt="" class="social-icon-small"></a>
-            <a href=""><img src="~/@/assets/discord.svg" alt="" class="social-icon-small"></a>
-          </div>
+                    <Social />
         </div>
       </div>
       <div class="wrapper">
