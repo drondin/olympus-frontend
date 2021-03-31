f64c71a7a41f6d7d9ed311693347842d25dfcc1a
Marcus <marcuseagan@gmail.com>
2021-03-30 17:07:39 -0700

    Update Social.vue
    to a non-broken link

--- src/components/Social.vue
+++ src/components/Social.vue
@@ -1,7 +1,7 @@
 <template>
 
      <div class="social-row">
-            <a href="https://github.com/OlympusDAO/olympus"><img src="~/@/assets/github.svg" alt="" class="social-icon-small"></a>
+            <a href="https://github.com/OlympusDAO/olympus-frontend"><img src="~/@/assets/github.svg" alt="" class="social-icon-small"></a>
             <a href="https://olympusdao.medium.com/"><img src="~/@/assets/medium.svg" alt="" class="social-icon-small"></a>
             <a href="https://twitter.com/OlympusDAO"><img src="~/@/assets/twitter.svg" alt="" class="social-icon-small"></a>
             <a href="https://discord.gg/6QjjtUcfM4"><img src="~/@/assets/discord.svg" alt="" class="social-icon-small"></a>
