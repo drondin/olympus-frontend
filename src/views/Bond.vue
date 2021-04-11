e66724dc18459c72badec77a86318117645ef588
unbanksy <jordan.alarkin@gmail.com>
2021-04-11 07:50:38 -0700

    Use Sidebar component within Bond.

--- src/views/Bond.vue
+++ src/views/Bond.vue
@@ -1,44 +1,7 @@
 <template>
   <div>
     <div id="dapp" class="dapp overflow-hidden">
-      <!-- <VueLoadingIndicator v-if="settings.loading" class="overlay big" />
-      <div v-else>
-      </div>-->
-      <div class="dapp-sidebar">
-
-        <div class="dapp-menu-top">
-          <div class="branding-header">
-            <router-link :to="{ name: 'home' }" class="">
-            <img class="branding-header-icon" src="~/@/assets/logo.svg" alt="">
-          </router-link>
-          </div>
-          <div class="wallet-menu">
-            <a v-if="address" class="disconnect-button button-primary button" @click="$store.state.settings.address = ''">Disconnect</a>
-          <a v-if="address" class="dapp-sidebar-button-connected button button-info">
-            <span class="login-bullet mr-2 ml-n2" />
-            {{ shorten(address) }}
-          </a>
-          <a v-else class="dapp-sidebar-button-connect button button-primary" @click="modalLoginOpen = true">
-            Connect wallet
-          </a>
-          </div>
-        </div>
-
-        <div class="dapp-menu-links">
-          <Dav />
-
-        </div>
-
-        <div class="dapp-menu-social">
-                    <Social />
-        </div>
-      </div>
-
-       <!-- <div class="wrapper">
-        <div class="dapp-center-modal">
-          <h1 style="line-height:25rem;">COMING SOON!</h1>
-        </div>
-      </div>  -->
+      <Sidebar />
 
        <div class="wrapper">
         <div class="dapp-center-modal">
@@ -161,14 +124,11 @@
         </div>
       </div>
     </div>
-    <ModalLogin :open="modalLoginOpen" @close="modalLoginOpen = false" />
-
   </div>
 </template>
 
 <script>
 import { mapState, mapActions } from 'vuex';
-import { shorten } from '@/helpers/utils.ts';
 import { ethers } from 'ethers';
 
 export default {
@@ -212,17 +172,10 @@ export default {
       },
       selectedMapOption: 'Bond',
       bondToggle: true,
-      modalLoginOpen: false,
     };
   },
   computed: {
     ...mapState(['settings']),
-
-    address() {
-      if(this.$store.state.settings.address)
-      return this.$store.state.settings.address
-      return null
-    },
 
     maxStrike() {
       const exchangeRate = this.settings.exchangeRates[this.form.asset];
@@ -261,14 +214,6 @@ export default {
   methods: {
 
     ...mapActions(['redeemBond', 'bondLP', 'forfeitBond', 'getLPBondApproval', 'getLPBondAllowance', 'calcBondDetails']),
-    disconnect() {
-      if(this.$store.state.settings.address)
-      return this.$store.state.address.initial
-      return null
-    },
-    shorten(addr) {
-      return shorten(addr);
-    },
 
     async setStake(value) {
       // Calculate suppliedQuantity and round it to avoid conflicts with uint.
