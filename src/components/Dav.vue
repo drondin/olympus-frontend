cf71ba5c920da16d678c4f39c1c4a4989387cba6
unbanksy <jordan.alarkin@gmail.com>
2021-04-15 16:10:07 -0700

    Reorder sidebar.

--- src/components/Dav.vue
+++ src/components/Dav.vue
@@ -1,21 +1,25 @@
 <template>
-       <div class="dapp-nav">
-        <router-link :to="{ name: 'migrate' }" class="button button-dapp-menu">
-          Reclaim AlphaOHM
-        </router-link>
-        <router-link :to="{ name: 'stake' }" class="button button-dapp-menu">
-          Stake
-        </router-link>
-        <router-link :to="{ name: 'lpstake' }" class="button button-dapp-menu">
-          LP Staking
-        </router-link>
-        <router-link :to="{ name: 'bond' }" class="button button-dapp-menu">
-          Bond
-        </router-link>
-        <router-link :to="{ name: 'dashboard' }" class="button button-dapp-menu">
-          Dashboard
-        </router-link>
-        </div>
+   <div class="dapp-nav">
+     <router-link :to="{ name: 'dashboard' }" class="button button-dapp-menu">
+       Dashboard
+     </router-link>
+
+     <router-link :to="{ name: 'stake' }" class="button button-dapp-menu">
+       Stake
+     </router-link>
+
+     <router-link :to="{ name: 'bond' }" class="button button-dapp-menu">
+       Bond
+     </router-link>
+
+    <router-link :to="{ name: 'lpstake' }" class="button button-dapp-menu">
+      LP Staking
+    </router-link>
+
+   <router-link :to="{ name: 'migrate' }" class="button button-dapp-menu">
+     Reclaim AlphaOHM
+   </router-link>
+ </div>
 
 </template>
 
