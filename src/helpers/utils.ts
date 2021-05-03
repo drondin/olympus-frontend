0a2e77708b9290d72510aee5dc869524f73de7a0
unbanksy <jordan.alarkin@gmail.com>
2021-05-03 16:44:48 -0700

    Fix issue when bond price is 77.

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -11,6 +11,9 @@ export function trim(number, precision) {
     number = 0;
   }
   const array = number.toString().split('.');
+  if (array.length === 1)
+    return number.toString();
+
   array.push(array.pop().substring(0, precision));
   const trimmedNumber = array.join('.');
   return trimmedNumber;
