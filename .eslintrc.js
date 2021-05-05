53c61f522a8e0f596610ab4f9f8e7a1ee4190522
unbanksy <jordan.alarkin@gmail.com>
2021-05-05 16:49:29 -0700

    Ignore errors on long lines.

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
     ecmaVersion: 2020
   },
   rules: {
+    'max-len': 0,
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     '@typescript-eslint/ban-ts-ignore': 'off'
