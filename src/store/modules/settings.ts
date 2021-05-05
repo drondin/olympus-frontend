1c4b6748ca07f90973d75f913eb3f5550242dead
unbanksy <jordan.alarkin@gmail.com>
2021-05-05 13:25:02 -0700

    Fixes issue with redeeming.

--- src/store/modules/settings.ts
+++ src/store/modules/settings.ts
@@ -673,7 +673,7 @@ const actions = {
     );
 
     try {
-      const redeemTx = await bonding.redeemBond();
+      const redeemTx = await bonding.redeem();
       await redeemTx.wait();
     } catch (error) {
       alert(error.message);
