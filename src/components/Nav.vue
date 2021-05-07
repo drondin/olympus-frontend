829f44f3dae606079406ec98db5ea2eb12a213ac
unbanksy <jordan.alarkin@gmail.com>
2021-05-06 22:38:37 -0700

    Add Get OHM button,

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,22 +1,22 @@
 <template>
-  <nav class="navbar navbar-dark bg-dark sticky-top py-2">
-    <div class="container-fluid">
+  <nav class="navbar navbar-dark bg-dark sticky-top py-2" style="width:100%;">
+    <div class="container-fluid" style="max-width: 1320px;">
       <router-link :to="{ name: 'home' }" class="navbar-brand d-flex align-items-center">
         <img src="~/@/assets/logo.svg" class="logo" />
         <h4 class="ticker-name">Olympus</h4>
         <h1 class="py-4"></h1>
       </router-link>
 
-      <div class="mainnav align-items-center">
-        <div class="drop-menu-item">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0 d-flex flex-row">
+        <li class="nav-item drop-menu-item">
           <h4>Developers</h4>
           <div class="drop-menu-hidden">
             <a href="https://docs.olympusdao.finance"><p>Documentation</p></a>
             <a href="https://github.com/olympusDAO"><p>Github</p></a>
           </div>
-        </div>
+        </li>
 
-        <div class="drop-menu-item">
+        <li class="nav-item drop-menu-item">
           <h4>Learn</h4>
           <div class="drop-menu-hidden">
             <a href="#what">
@@ -29,9 +29,9 @@
               <p>Blog</p>
             </a>
           </div>
-        </div>
+        </li>
 
-        <div class="drop-menu-item">
+        <li class="nav-item drop-menu-item">
           <h4>Governance</h4>
           <div class="drop-menu-hidden">
             <a href="http://vote.olympusdao.finance/">
@@ -41,26 +41,22 @@
               <p>Forum</p>
             </a>
           </div>
-        </div>
+        </li>
 
-        <router-link
-          :to="{ name: 'stake' }"
-          class="button button-primary d-none d-sm-block mx-4"
-          style="color: black !important;"
-        >
-          Enter App
-        </router-link>
+      </ul>
 
-        <!-- <div class="drop-menu-item">
-          <h4>Community</h4>
-          <div class="drop-menu-hidden">
-            <a href="https://twitter.com/OlympusDAO"><p>Twitter</p></a>
-            <a href="https://discord.gg/tmbtUpR6CF"><p>Discord</p></a>
-            <a href="https://olympusdao.medium.com/"><p>Blog</p></a>
-          </div>
-        </div> -->
-      </div>
+      <router-link :to="{ name: 'stake' }" class="button button-primary d-none d-sm-block mx-4" style="color: black !important;">
+        Enter App
+      </router-link>
+
+      <a role="button" class="btn btn-dark btn-overwrite-primer" href="https://app.sushi.com/swap?inputCurrency=0x6b175474e89094c44da98b954eedeac495271d0f&outputCurrency=0x383518188c0c6d7730d91b2c03a03c837814a899" target="_blank">
+        Get OHM
+      </a>
     </div>
+
+
+
+
 
 
     <ModalLogin :open="modalLoginOpen" @close="modalLoginOpen = false" />
