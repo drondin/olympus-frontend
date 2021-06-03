878690b725df169f92c653300b5be3c32ecbe4ac
ZayenX <zayenx@gmail.com>
2021-06-02 19:47:53 -0700

    lowercase v

--- src/components/Dav.vue
+++ src/components/Dav.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="dapp-nav">
-    <a href="https://app.olympusdao.finance/" class="button button-dapp-menu"> Go to main dashboard </a>
+    <a href="https://app.olympusdao.finance/" class="button button-dapp-menu"> Go to v1.1 </a>
 
     <router-link :to="{ name: 'lpstake' }" class="button button-dapp-menu">
       LP Staking
