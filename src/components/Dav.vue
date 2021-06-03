4b062d562ea2b1d3297c17e5a1f121687ad1181d
ZayenX <zayenx@gmail.com>
2021-06-02 19:44:03 -0700

    remove routing values provide link to old dashboard

--- src/components/Dav.vue
+++ src/components/Dav.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="dapp-nav">
+    <a href="https://app.olympusdao.finance/" class="button button-dapp-menu"> Go to main dashboard </a>
+
     <router-link :to="{ name: 'lpstake' }" class="button button-dapp-menu">
       LP Staking
     </router-link>
--- src/router.ts
+++ src/router.ts
@@ -1,13 +1,6 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
 import Home from '@/views/Home.vue';
-import Dapp from './views/Dapp.vue';
-import Dashboard from './views/Dashboard.vue';
-import Stake from './views/Stake.vue';
-import Bond from './views/Bond.vue';
-import ChooseBond from './views/ChooseBond.vue';
-import DaiBond from './views/DaiBond.vue';
-import Migrate from './views/Swap.vue';
 import LPStaking from './views/LPStaking.vue';
 import store from './store/index';
 
@@ -15,14 +8,8 @@ Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   { path: '/', name: 'home', component: Home },
-  { path: '/dashboard', name: 'dashboard', component: Dashboard },
-  { path: '/stake', name: 'stake', component: Stake },
-  { path: '/bond', name: 'bond', component: Bond },
-  { path: '/bondai', name: 'bondai', component: DaiBond },
-  { path: '/choose_bond', name: 'choose_bond', component: ChooseBond },
   { path: '/lpstake', name: 'lpstake', component: LPStaking },
-  { path: '/migrate', name: 'migrate', component: Migrate },
-  { path: '/app', name: 'dapp', component: Dapp }
+
 ];
 
 const router = new VueRouter({
