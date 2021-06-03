99a4521356ed3480bf8a6a1729ec61adca558819
ZayenX <zayenx@gmail.com>
2021-06-02 19:33:01 -0700

    removing other options from V1

--- src/components/Dav.vue
+++ src/components/Dav.vue
@@ -1,17 +1,5 @@
 <template>
   <div class="dapp-nav">
-    <a href="https://app.olympusdao.finance/#/dashboard" class="button button-dapp-menu">
-      Dashboard
-    </a>
-
-    <a href="https://app.olympusdao.finance/#/" class="button button-dapp-menu">
-      Stake
-    </a>
-
-    <a href="https://app.olympusdao.finance/#/bonds" class="button button-dapp-menu">
-      Bond
-    </a>
-
     <router-link :to="{ name: 'lpstake' }" class="button button-dapp-menu">
       LP Staking
     </router-link>
